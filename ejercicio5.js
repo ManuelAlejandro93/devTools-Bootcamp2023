997dac186cbf4f8907ee3c2407dad3dcbfc4ecbd
Manuel Alejandro <clasefisica2019@gmail.com>
2023-06-08 15:59:54 -0500

    Ejercicio5 completado

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,25 +1,42 @@
-let n = 3;
-let stringVacio="";
+function drawX (n){
 
-for(let i=0;i<n;i++){
-    stringVacio += " ";
+    let stringVacio="";
+    let arrayAModificar;
+    let arrayRespaldo;
+
+    if(n%2===0){
+        n--;
+        console.log(`Tu numero fue aproximado a ${n}.`);
+    } else if (n<=0){
+        return "Ingrese numeros enteros mayores a 0"
+    } else if (!Number.isInteger(n)){
+        n=Math.floor(n);
+        if(n%2===0){
+            n--;
+        }
+
+        console.log(`Tu numero fue aproximado a ${n}`);
+    }
+
+
+    for(let i=0;i<n;i++){
+        stringVacio += " ";
+    }
+
+    arrayAModificar = stringVacio.split("");
+    arrayRespaldo=[...arrayAModificar];
+
+
+    for(let i=0;i<arrayAModificar.length;i++){
+        let cadenaAImprimir="";
+        arrayAModificar[i]="X";
+        arrayAModificar[arrayAModificar.length-1-i]="X";
+        cadenaAImprimir = arrayAModificar.join("");
+        console.log(cadenaAImprimir);
+        arrayAModificar=[...arrayRespaldo];
+    }
+
+    return null;
 }
 
-// pasa.
-
-
-let arrayVacio = stringVacio.split("");
-//pasa.
-
-let arrayRespaldo=[...arrayVacio];
-//pasa.
-
-
-for(let i=0;i<arrayVacio.length;i++){
-    let cadenaAImprimir;
-    arrayVacio[i]="X";
-    arrayVacio[arrayVacio.length-1-i]="X";
-    cadenaAImprimir = arrayVacio.join("");
-    console.log(cadenaAImprimir);
-    arrayVacio=[...arrayRespaldo];
-}
+drawX(21);
